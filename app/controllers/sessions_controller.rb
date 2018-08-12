c2e34b1233368b0202ffbff88fd1a5f7dbdc1a8c
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-12 00:50:04 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,9 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: params[:name])
-    return head(:forbidden) unless @user.authenticate(params[:password])
+    @user && @user.authenticate(params[:password])
     session[:user_id] = @user.id
+    return head(:forbidden) unless @user.authenticate(params[:password])
   end
 
   def destroy
