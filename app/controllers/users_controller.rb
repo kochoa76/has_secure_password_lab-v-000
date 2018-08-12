9cd42585ed0e05861260e702453376f06a2c367e
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-12 00:58:48 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     if @user && @user.authenticate(params[:password])
     session[:user_id] = @user.id
     redirect_to controller: 'users/welcome'
-  end 
+    end 
   end
 
   private
