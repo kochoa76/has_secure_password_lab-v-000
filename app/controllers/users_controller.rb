e327ed2e3960f0ebbf3cc48a0694914876260e09
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-12 00:56:05 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 
   def create
     @user= User.create(user_params)
+    @user && @user.authenticate(params[:password])
     session[:user_id] = @user.id
     redirect_to controller: 'users/welcome' 
 
